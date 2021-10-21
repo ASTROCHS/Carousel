8d98cfaf6a2a0e6dbbff5e1f0dd9a7473a4c22c6
ASTROCHS <90197039+ASTROCHS@users.noreply.github.com>
2021-10-22 01:59:07 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+var answer = ['images/image-1.png', 'images/image-2.png', 'images/image-3.png', 'images/image-4.png', 'images/image-5.png'];
+var getImage = document.querySelector('.main_image');
+var leftArrow = document.querySelector('.left');
+var rightArrow = document.querySelector('.right');
+
+rightArrow.addEventListener('click',button_right);
+leftArrow.addEventListener('click',button_left);
+
+var numIndex = answer.indexOf(getImage.getAttribute('src'));
+
+function button_right(){
+  if (numIndex < answer.length-1){
+  numIndex++;
+  } else {
+  numIndex = 0;
+  }
+  getImage.src = answer[numIndex];
+}
+
+function button_left(){
+  if (numIndex > 0){
+  numIndex--;
+  } else {
+  numIndex= 4;
+  }
+  getImage.src = answer[numIndex];
+}
+
+var target = document.querySelectorAll('.dot');
+var targetLength = target.length;
+
+for(let i = 0; i<targetLength; i++){
+  target[i].addEventListener('click',function(){dot_click(i)})
+}
+
+function dot_click(x){
+  getImage.src = answer[x];
+  target[x].classList.add('active');
+}
+
